df181f9863db2e66112c5ee527af7f4bd6ee8bb3
DavidKniotek <dawid.kniotek@gmail.com>
2023-01-26 17:05:13 +0100

    IP restriction has been added

--- routes/votes.js
+++ routes/votes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const voteRouter = express.Router();
+const {IpRestrict} = require('../utils/ip-restrict')
 const INFO = 'Thank you for your vote!';
 
 const votes = {
     yes: 0,
     no: 0,
 };
+
+const isIpAlreadyVoted = new IpRestrict();
 
 voteRouter
 
@@ -15,6 +18,12 @@ voteRouter
     })
 
     .get('/:voteResult', (req, res) => {
+
+        if (isIpAlreadyVoted.check(req.ip)) {
+            res.status(403).send('You already have voted in this survey!');
+            return;
+        }
+
         const {voteResult} = req.params;
         if (typeof votes[voteResult] === 'undefined') {
             votes[voteResult] = 0;
